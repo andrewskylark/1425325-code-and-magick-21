47ee6831eee6afd73579b9249c35e87b502439aa
andrewskylark <andrew.skylark@gmail.com>
2020-11-01 10:24:01 +0400

    код превьюшки аватара

--- js/debounce.js
+++ js/debounce.js
@@ -1,17 +1,4 @@
 'use strict';
-// (function () {
-//   const DEBOUNCE_INTERVAL = 500; // ms
-
-//   let lastTimeout;
-//   window.debounce = function (cb) {
-//     if (lastTimeout) {
-//       window.clearTimeout(lastTimeout);
-//     }
-//     lastTimeout = window.setTimeout(cb, DEBOUNCE_INTERVAL);
-//   };
-// })();
-// Файл debounce.js
-
 (function () {
   let DEBOUNCE_INTERVAL = 500; // ms
 
